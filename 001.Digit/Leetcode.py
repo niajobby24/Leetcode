1baa10f0353465b822ff02016b9ef81e6797232f
ArvinderPal10 <arvinderpal10@gmail.com>
2025-08-07 00:00:37 +0530

    Leetcode questions - Two Pointers

---
+++ 001.Digit/Leetcode.py
@@ -0,0 +1,26 @@
+"""
+258. Add Digits
+
+1281. Subtract the Product and Sum of Digits of an Integer
+
+1295. Find Numbers with Even Number of Digits
+
+1796. Second Largest Digit in a String
+
+1945. Sum of Digits of String After Convert
+
+2180. Count Integers With Even Digit Sum 
+
+2283. Check if Number Has Equal Digit Count and Digit Value
+
+2520. Count the Digits That Divide a Number
+
+2535. Difference Between Element Sum and Digit Sum of an Array
+
+2544. Alternating Digit Sum
+
+2553. Separate the Digits in an Array
+
+3536. Maximum Product of Two Digits
+
+"""
